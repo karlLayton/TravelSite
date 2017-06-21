460151aaee86a673d2614d0e00a8c9b0d0b437c6
Karl <karlwlayton@gmail.com>
2017-06-21 08:54:30 -0400

    Organizing files for project, adding the start of the form

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('mainApp', ['ngAnimate', 'ui-router']);
+app.controller('pageCtrl', function($scope, $http) {
+    $http.get("templates/header.html").then(function (response) {
+        $scope.names = response.data.records;
+    });
+    $http.get("partials/home.html").then(function (response) {
+        $scope.names = response.data.records;
+    });
+    $http.get("templates/footer.html").then(function (response) {
+        $scope.names = response.data.records;
+    });
+});
